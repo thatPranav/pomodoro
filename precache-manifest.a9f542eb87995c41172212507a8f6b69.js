679a8633547e782b3cd7d02ad9f0c4e0e9eb90b7
thatPranav <jawajipranav@gmail.com>
2021-06-29 10:56:24 +0530

    Updates

---
+++ precache-manifest.a9f542eb87995c41172212507a8f6b69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9356c161e48bc3f4d8b3f0b4c789d4d3",
+    "url": "/pomodoro/index.html"
+  },
+  {
+    "revision": "b519129a86d1215c1018",
+    "url": "/pomodoro/static/css/main.540a1c28.chunk.css"
+  },
+  {
+    "revision": "a83b866c1c0f576d1986",
+    "url": "/pomodoro/static/js/2.5b385fb7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodoro/static/js/2.5b385fb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b519129a86d1215c1018",
+    "url": "/pomodoro/static/js/main.1f501f37.chunk.js"
+  },
+  {
+    "revision": "4f8a8d18ba8fe990dabb",
+    "url": "/pomodoro/static/js/runtime-main.dddcbe64.js"
+  }
+]);
